c59141f5d6a5482152c1a0f267f61f0b785f0b79
jugglingdev <kaylamarie1785@gmail.com>
2024-02-05 20:05:23 -0600

    Create Users controller and configure routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+    
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
+  def show
+    render json: @user, status: 200
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: nil, status: :ok
+    else
+      redner json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def posts_index
+    user = User.find(params[:user_id])
+
+    user_posts = user.posts
+    render json: user_posts, status: :ok
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:email, :password)
+  end
+end
