907eaaa42ee9402f743b097632ea4f94a9468fc3
jugglingdev <kaylamarie1785@gmail.com>
2024-02-05 18:32:01 -0600

    Create User class

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    # validations
+    validates :email, presence: true, uniqueness: true, length: { minimum: 5, maximum: 255 },
+        format: {
+            with: URI::MailTo::EMAIL_REGEXP
+        }
+    validates :password, presence: true, length: { minimum: 12, maximum: 50 }
+
+    # associations
+    
+end
