66066cc7fd9e433be5e0bc4d3fe9407c985fb32b
jugglingdev <kaylamarie1785@gmail.com>
2024-02-09 20:25:58 -0600

    WIP

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,4 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
+    context 'validation tests' do
+        it 'is not valid without a name' do
+            tag = build(:tag, name: nil);
+            expect(tag).not_to be_valid
+        end
+    
+        it 'has a name shorter than 50 characters' do
+            tag = build(:tag, name: Faker::Lorem.paragraph_by_chars(number: 51, supplemental: false))
+            expect(tag).not_to be_valid
+        end
+    end
+
+    context 'association tests' do
+        it 'belongs to a carousel' do
+            tag = build(:tag)
+            expect(tag.carousel).to be_present
+        end
+
+        it 'has many carousels' do
+            tag = create(:tag)
+            create_list(:carousel, 3, tag: tag)
+    
+            tag.reload
+            expect(tag.carousels.count).to eq(3)
+        end
+    end
+
 end
