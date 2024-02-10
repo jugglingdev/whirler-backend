4d894d2ab5a1264c19dd71fd070bd441b6147cf9
jugglingdev <kaylamarie1785@gmail.com>
2024-02-09 20:23:49 -0600

    Update tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
     # validations
-    validates :name, length: { maximum: 50 }
+    validates :name, presence: true, length: { maximum: 50 }
 
     # associations
     has_and_belongs_to_many :carousels
