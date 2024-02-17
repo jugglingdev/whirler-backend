f637671e116bb36848c1e4a1465901747006f4e4
jugglingdev <kaylamarie1785@gmail.com>
2024-02-16 18:36:46 -0600

    Add first name and last name columns to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_221548) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_16_010930) do
   create_table "carousels", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -58,6 +58,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_221548) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
   end
 
   add_foreign_key "carousels", "users"
